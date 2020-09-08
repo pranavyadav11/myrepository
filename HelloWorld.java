9fe63b5cbe439d4835f695992833e7ca0932f7f9
pranav yadav <pranav@gmail.com>
2020-09-08 11:31:10 +0000

    working

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
-class HelloWorld {
-    public static void main(String[] args) {
-    	System.out.println("Updated")
-    }
-}
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
+
